1b77266033cbaea21287c012c686211add966831
정원도 <yarang@megagen.co.kr>
2017-07-19 09:09:26 +0900

    Adding json parsing check user id

--- gdrive-telegram-bot/gdrive_telegram_bot.py
+++ gdrive-telegram-bot/gdrive_telegram_bot.py
@@ -3,6 +3,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import telegram 
 from subprocess import PIPE, STDOUT, Popen, check_output
 import shlex
+import json
 
 import httplib2
 import os
@@ -19,12 +20,16 @@ try:
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 except ImportError:
     flags = None
+
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/drive-python-quickstart.json
 SCOPES = 'https://www.googleapis.com/auth/drive'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Drive API Python Quickstart'
 proc = None
+
+with open('users.json') as user_file:
+	jsondata = json.load(user_file)
 
 def get_credentials():
     """Gets valid user credentials from storage.
@@ -55,7 +60,12 @@ def get_credentials():
     return credentials
 
 def start(bot, update):
-    update.message.reply_text('Hello World!')
+	for user in jsondata["users"]:
+		id = user["id"]
+		if(id == update.message.from_user.id):
+			update.message.reply_text('Hello World!')
+		else:
+			update.message.reply_text('Sorry!')
 
 def hello(bot, update):
     update.message.reply_text(
